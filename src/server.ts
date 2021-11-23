1877fc4f1964d1461de8ff48b0db020ab0b96a7e
Ykoh <ykoh@student.42seoul.kr>
2021-11-23 10:05:00 +0900

    Fix: console.log를 logger로 대체
    See also: #14

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { app } from './app';
+import { logger } from './config/logger';
 import { sequelize } from './config/sequelize';
 
 const port = process.env.PORT || 5000;
@@ -6,5 +7,5 @@ const port = process.env.PORT || 5000;
 (async () => {
   await sequelize.sync({ force: true });
 
-  app.listen(port, () => console.log(`Server listen on port ${port}`));
+  app.listen(port, () => logger.debug(`Server listen on port ${port}`));
 })();
