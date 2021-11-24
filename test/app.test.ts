af644998aea78eee269b881a3f187e459c619d8e
Ykoh <ykoh@student.42seoul.kr>
2021-11-24 20:09:01 +0900

    Fix: 테스트시 db가 연결되어있지 않았던 문제 수정

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,7 +1,12 @@
 import request from 'supertest';
 import { app } from '../src/app';
+import { sequelize } from '../src/database/sequelize';
 
 describe('GET /', () => {
+  beforeAll(async () => {
+    await sequelize.sync();
+  });
+
   it('should return 404', (done) => {
     request(app).get('/dd').expect(404, done);
   });
