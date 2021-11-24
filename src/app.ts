1e09266bbc349ac5a394f4e2573acb4bae9a9478
Ykoh <ykoh@student.42seoul.kr>
2021-11-24 18:22:40 +0900

    Fix: 클러스터 목록을 db에서 가져오도록 수정
    capacity는 임시 값 부여
    
    Related to: #16

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { engine } from 'express-handlebars';
 import morgan from 'morgan';
 import { winstonStream } from './config/logger';
 import { capacityBadgeBgColor } from './config/helpers';
+import { Cluster } from './clusters/cluster.model';
 
 export const app = express();
 
@@ -19,11 +20,8 @@ app.engine(
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, '../views'));
 
-app.get('/', (_req, res) => {
-  const clusters = [
-    { name: '개포', capacity: 100, max: 150 },
-    { name: '서초', capacity: 1, max: 200 },
-    { name: '성수', capacity: 20, max: 20 },
-  ];
+app.get('/', async (_req, res, _next) => {
+  const clusters: Cluster[] = await Cluster.findAll({ raw: true });
+  clusters.map((cluster: any) => (cluster.capacity = 10)); // STUB capacity용 임시코드 + any type interface 확장
   res.render('home', { title: 'home', clusters });
 });
--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import { sequelize } from './database/sequelize';
 const port = process.env.PORT || 5000;
 
 (async () => {
-  await sequelize.sync({ force: true });
+  await sequelize.sync({ force: false });
 
   app.listen(port, () =>
     logger.debug(`Server listen on port ${port} in ${STAGE} stage!`),
