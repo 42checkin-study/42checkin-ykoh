1ec646adfa5ad84cdc445390f179a00ff2940ca6
Ykoh <ykoh@student.42seoul.kr>
2021-12-03 02:39:03 +0900

    Test: CI 통과하도록 테스트 코드 작성

--- test/check-in.test.ts
+++ test/check-in.test.ts
@@ -6,11 +6,19 @@ beforeAll(async () => {
   await sequelize.sync();
 });
 
-describe('GET /checkin', () => {
-  it('should return 404', (done) => {
-    request(app).get('/').expect(404, done);
+describe('GET /', () => {
+  it('should return 302', (done) => {
+    request(app).get('/').expect(302, done);
   });
+});
 
+describe('GET /signin', () => {
+  it('should return 200', (done) => {
+    request(app).get('/signin').expect(200, done);
+  });
+});
+
+describe('GET /checkin', () => {
   it('should return 200', (done) => {
     request(app).get('/checkin').expect(200, done);
   });
